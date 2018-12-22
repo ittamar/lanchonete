636355cf663cce212b7b8697683b20e824e5d43c
itamar <itamargomes038@gmail.com>
2018-12-22 10:42:41 -0300

    formulario de criar sabores de pizza

--- app/controllers/criarCardapio.js
+++ app/controllers/criarCardapio.js
@@ -2,10 +2,11 @@ module.exports.criarCardapio = function(aplication,req,res){
   //conecatr com o banco
 //  var connection = aplication.infra.connectionFactory();
   //var conectandoBancoLanchonete = new aplication.model.lancheModel(connection);
-  if (req.session.autorizado != undefined) {
+
+  /*if (req.session.autorizado != undefined) {
     res.render("lanchonete/criar_cardapio");
       }else {
           res.render('lanchonete/login');
-        }
-
+        }*/
+res.render("lanchonete/criar_cardapio");
 }
